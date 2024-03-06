61cc7dd5ba2983eee8a9b0889b1085973f35e8c8
10urkarasu <10urkarasu@gmail.com>
2024-03-06 20:10:15 +0300

    added auth

--- src/pages/applayout/AppLayout.jsx
+++ src/pages/applayout/AppLayout.jsx
@@ -1,14 +1,16 @@
 import Map from "../../components/map/Map";
 import SideBar from "../../components/sidebar/SideBar";
+import User from "../../components/user/User";
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
-  return (
-    <div className={styles.app}>
-      <SideBar />
-      <Map />
-    </div>
-  );
+    return (
+        <div className={styles.app}>
+            <SideBar />
+            <Map />
+            <User />
+        </div>
+    );
 }
 
 export default AppLayout;
