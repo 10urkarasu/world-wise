e07a0c5e890896bee5b1c8239c0811fd267e29b3
10urkarasu <10urkarasu@gmail.com>
2024-03-23 17:47:01 +0300

    bug fixed

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -4,14 +4,14 @@ import AppNav from "../appnav/AppNav";
 import SideBarFooter from "../sidebarfooter/SideBarFooter";
 import { Outlet } from "react-router-dom";
 function SideBar() {
-  return (
-    <div className={styles.sidebar}>
-      <Logo />
-      <AppNav />
-      <Outlet />
-      <SideBarFooter />
-    </div>
-  );
+    return (
+        <div className={styles.sidebar}>
+            <Logo />
+            <AppNav />
+            <Outlet />
+            <SideBarFooter />
+        </div>
+    );
 }
 
 export default SideBar;
